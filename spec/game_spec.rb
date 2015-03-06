e31b20aad40660f60e97f3c193453e7e86e4ea53
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-03-06 02:30:46 +0000

    game: tests refactored

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,13 @@
 require 'game'
 
 describe Game do
-  it 'has no grid when created' do
-    game = Game.new
+  let(:game) { Game.new }
 
+  it 'has no grid when created' do
     expect(game).not_to have_grid
   end
 
   it 'can have a grid' do
-    game = Game.new
-
     game.grid = :grid
 
     expect(game).to have_grid
