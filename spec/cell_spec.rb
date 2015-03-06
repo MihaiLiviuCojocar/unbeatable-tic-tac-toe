0d8f5e646feed77fcbe3b24aacfd33d3ad0db694
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-03-06 02:20:01 +0000

    cell: tests refactored

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,15 +1,13 @@
 require 'cell'
 
 describe Cell do
-  it 'has no content when created' do
-    cell = Cell.new
+  let(:cell) { Cell.new }
 
+  it 'has no content when created' do
     expect(cell).not_to have_content
   end
 
   it 'can have content' do
-    cell = Cell.new
-
     cell.content = :cross
 
     expect(cell).to have_content
