69a5b26d08dc4c6bf0d70389df7d98e7e9bf2a4e
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-03-06 08:33:14 +0000

    game: tests refactored

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,14 @@
 require 'game'
 
 describe Game do
-  let(:game)       { Game.new }
-  let(:game_ready) { Game.new }
+  let(:game)       { Game.new                                      }
+  let(:game_ready) { Game.new                                      }
+  let(:player_one) { double :player, winner?: false, name: 'Mihai' }
+  let(:player_two) { double :player, winner?: false                }
 
   before(:each) do
-    game_ready.add_player(:player_one)
-    game_ready.add_player(:player_two)
+    game_ready.add_player(player_one)
+    game_ready.add_player(player_two)
     game_ready.grid = :grid
   end
 
@@ -46,17 +48,11 @@ describe Game do
   end
 
   it 'cannot add players if it already has two players' do
-    game.add_player(:player_one)
-    game.add_player(:player_two)
-
-    expect{game.add_player(:player_three)}.to raise_error(GameFullError, 'The game is full!')
+    expect{game_ready.add_player(:player_three)}.to raise_error(GameFullError, 'The game is full!')
   end
 
   it 'knows when it has two players' do
-    game.add_player(:player_one)
-    game.add_player(:player_two)
-
-    expect(game).to have_two_players
+    expect(game_ready).to have_two_players
   end
 
   it 'is ready to start when it has a grid and two players' do
@@ -64,53 +60,36 @@ describe Game do
   end
 
   it 'knows that player one has the first move' do
-    expect(game_ready.current_player).to eq(:player_one)
+    expect(game_ready.current_player).to eq(player_one)
   end
 
   it 'can switch turns' do
-    player_one = double :player, winner?: false
-    player_two = double :player, winner?: false
-    game.add_player(player_one)
-    game.add_player(player_two)
+    game_ready.switch_turns
 
-    game.switch_turns
-
-    expect(game.current_player).to eq(player_two)
+    expect(game_ready.current_player).to eq(player_two)
   end
 
   it 'knows that player one can be the winner' do
-    player_one = double :player, winner?: true
-    player_two = double :player, winner?: false
-    game.add_player(player_one)
-    game.add_player(player_two)
+    allow(player_one).to receive(:winner?).and_return(true)
 
-    expect(game.winner).to eq(player_one)
+    expect(game_ready.winner).to eq(player_one)
   end
 
   it 'knwos that player two can be the winner' do
-    player_one = double :player, winner?: false
-    player_two = double :player, winner?: true
-    game.add_player(player_one)
-    game.add_player(player_two)
+    allow(player_two).to receive(:winner?).and_return(true)
 
-    expect(game.winner).to eq(player_two)
+    expect(game_ready.winner).to eq(player_two)
   end
 
   it 'is over if there is a winner' do
-    player_one = double :player, winner?: false
-    player_two = double :player, winner?: true
-    game.add_player(player_one)
-    game.add_player(player_two)
+    allow(player_two).to receive(:winner?).and_return(true)
 
-    expect(game).to be_over
+    expect(game_ready).to be_over
   end
 
   it 'cannot switch turns if there is a winner' do
-    player_one = double :player, winner?: true, name: 'Mihai'
-    player_two = double :player, winner?: false
-    game.add_player(player_one)
-    game.add_player(player_two)
+    allow(player_one).to receive(:winner?).and_return(true)
 
-    expect{game.switch_turns}.to raise_error('Game over! Mihai wins!')
+    expect{game_ready.switch_turns}.to raise_error('Game over! Mihai wins!')
   end
 end
