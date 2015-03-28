e058836dd1fd07a6d93f8b0042af6280eed3b99d
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-03-28 16:41:41 +0000

    grid: can be two by two

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,2 +1,22 @@
 class Grid
+  DEFAULT_SIZE = 1
+
+  def initialize(size: DEFAULT_SIZE)
+    @matrix = matrix_builder(size)
+  end
+
+  def coordinates
+    @matrix.keys
+  end
+
+  private
+
+  def matrix_builder(size)
+    [*'A'..equivalent_letter_for(size)].map{ |letter| [*1..size].map { |number| {"#{letter}#{number}".to_sym => nil } } }.flatten.reduce(:merge)
+  end
+
+  def equivalent_letter_for(number)
+    (number + 64).chr
+  end
+
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,4 +1,13 @@
 require 'grid'
 
 describe Grid do
+  it 'can be one by one' do
+    grid = Grid.new
+    expect(grid.coordinates).to eq [:A1]
+  end
+
+  it 'can be two by two' do
+    grid = Grid.new(size: 2)
+    expect(grid.coordinates).to eq [:A1, :A2, :B1, :B2]
+  end
 end
