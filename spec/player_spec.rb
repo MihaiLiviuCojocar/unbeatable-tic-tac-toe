acc8b68ba8a17e2374844b8c9c679d32990f1c7f
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-03-28 16:02:56 +0000

    player: refactored player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,62 +1,62 @@
 require 'player'
 
 describe Player do
-  let(:grid)   { double :grid                          }
-  let(:player) { Player.new(name: 'Mihai', grid: grid) }
+  let(:grid)                 { double :grid                               }
+  let(:solutions_calculator) { double :solutions_calculator               }
+  let(:player)               { Player.new(name: 'Mihai', grid: grid)      }
 
-  it 'has a name when created' do
-    expect(player.name).to eq('Mihai')
+  context 'when created' do
+    it 'has a name when created' do
+      expect(player.name).to eq('Mihai')
+    end
+
+    it 'has a grid when created' do
+      expect(player.grid).to eq grid
+    end
+
+    it 'is not a winner when created' do
+      expect(player).not_to be_winner
+    end
+
+    it 'can have a marker' do
+      player.marker = :cross
+
+      expect(player.marker).to eq(:cross)
+    end
+
+    it 'has a solutions calculator' do
+      player.solutions_calculator = solutions_calculator
+      expect(player.solutions_calculator).to eq solutions_calculator
+    end
   end
 
-  it 'has a grid when created' do
-    expect(player.grid).to eq grid
-  end
+  context 'making a move' do
+    before do
+      player.marker = :zerro
+      player.solutions_calculator = solutions_calculator
+    end
 
-  it 'is not a winner when created' do
-    expect(player).not_to be_winner
-  end
+    it 'can place his marker on the grid at a coordinate' do
+      allow(solutions_calculator).to receive(:winning_solutions).and_return([])
 
-  it 'can have a marker' do
-    player.marker = :cross
+      expect(grid).to receive(:place_marker).with(:A1, :zerro)
 
-    expect(player.marker).to eq(:cross)
-  end
+      player.place_marker(:A1)
+    end
 
-  it 'can place his marker on the grid at a coordinate' do
-    coordinate    = :A1
-    player.marker = :zerro
-    solutions_calculator        = double :solutions_calculator
-    player.solutions_calculator = solutions_calculator
-    allow(solutions_calculator).to receive(:winning_solutions).and_return([])
+    it 'knwos the winning solutions' do
+      expect(solutions_calculator).to receive(:winning_solutions)
 
-    expect(grid).to receive(:place_marker).with(coordinate, :zerro)
+      player.winning_solutions
+    end
 
-    player.place_marker(coordinate)
-  end
+    it 'knows that he wins if he marks a coordinate which is a solution' do
+      allow(solutions_calculator).to receive(:winning_solutions).and_return([:A3])
+      allow(grid).to receive(:place_marker).with(:A3, :zerro)
 
-  it 'has a solutions calculator' do
-    player.solutions_calculator = :solutions_calculator
-    expect(player.solutions_calculator).to eq :solutions_calculator
-  end
+      player.place_marker(:A3)
 
-  it 'knwos the winning solutions' do
-    solutions_calculator        = double :solutions_calculator
-    player.solutions_calculator = solutions_calculator
-
-    expect(solutions_calculator).to receive(:winning_solutions)
-
-    player.winning_solutions
-  end
-
-  it 'knows that he wins if he marks a coordinate which is a solution' do
-    solutions_calculator        = double :solutions_calculator
-    player.solutions_calculator = solutions_calculator
-    player.marker               = :zerro
-    allow(solutions_calculator).to receive(:winning_solutions).and_return([:A3])
-    allow(grid).to receive(:place_marker).with(:A3, :zerro)
-
-    player.place_marker(:A3)
-
-    expect(player).to be_winner
+      expect(player).to be_winner
+    end
   end
 end
