71bd58221af8a17d0927847bf1ef0eede74f15e6
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-04-06 10:36:50 +0100

    changed a bit player.ask_for_recommendation()

--- lib/player.rb
+++ lib/player.rb
@@ -25,11 +25,10 @@ class Player
 
   def ask_for_recommendation
     case moves_count
-    when 0 then solutions_calculator.first_move_recommendation
-    when 1 then solutions_calculator.second_move_recommendation
-    when 2 then solutions_calculator.third_move_recommendation
-    else
-      solutions_calculator.recommendation
+      when 0 then solutions_calculator.first_move_recommendation
+      when 1 then solutions_calculator.second_move_recommendation
+      when 2 then solutions_calculator.third_move_recommendation
+      else solutions_calculator.recommendation
     end
   end
 
