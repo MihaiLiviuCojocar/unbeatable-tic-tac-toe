900f1b637b2397fa75f6712c5d3de31121b59a21
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-04-30 18:46:08 +0100

    rolled back to hard coded POSSIBLE_COMBINATIONS due to decreased speed when it is dynamic

--- lib/solutions_calculator.rb
+++ lib/solutions_calculator.rb
@@ -1,4 +1,15 @@
 class SolutionsCalculator
+  POSSIBLE_COMBINATIONS = [
+    [:A1, :B1, :C1],
+    [:A2, :B2, :C2],
+    [:A3, :B3, :C3],
+    [:A1, :A2, :A3],
+    [:B1, :B2, :B3],
+    [:C1, :C2, :C3],
+    [:A1, :B2, :C3],
+    [:A3, :B2, :C1]
+  ]
+
   attr_reader :game, :marker
 
   def initialize(options = {})
@@ -11,7 +22,7 @@ class SolutionsCalculator
   end
 
   def possible_combinations
-    coordinates_of_possible_combinations.map do |row|
+    POSSIBLE_COMBINATIONS.map do |row|
       Row.new(cells: row.map { |coordinate| grid.matrix[coordinate] }, marker: marker)
     end
   end
@@ -29,39 +40,6 @@ class SolutionsCalculator
   end
 
   private
-
-  def coordinates_of_possible_combinations
-    row1 = get_vertical_rows
-    row2 = get_horizantal_rows
-
-    sol1 = []
-    sol2 = []
-    
-    row1.each_with_index do |item, index|
-      sol1 << item[index]
-    end
-
-    row2.each_with_index do |item, index|
-      sol2 << item.reverse[index]
-    end
-
-    solutions =  get_horizantal_rows
-    solutions += get_vertical_rows
-    solutions << sol1
-    solutions << sol2
-  end
-
-  def get_horizantal_rows
-    grid.matrix.keys.map do |key|
-      grid.matrix.keys.select do |coord|
-        coord.to_s.chars.last == key.to_s.chars.last
-      end
-    end.uniq
-  end
-
-  def get_vertical_rows
-    get_horizantal_rows.transpose
-  end
 
   def get_key_of(cell)
     grid.matrix.key(cell)
