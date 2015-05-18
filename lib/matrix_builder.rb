11cbdacfde4c8076d4efb6f38d58c5fce88411c4
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2015-05-18 13:33:26 +0100

    more cleaning ...

--- lib/matrix_builder.rb
+++ lib/matrix_builder.rb
@@ -1,8 +1,10 @@
 module MatrixBuilder
+  DEFAULT_VALUE = nil
+
   def build_matrix(size)
     [*'A'..equivalent_letter_for(size)].map do |letter|
       [*1..size].map do |number|
-        { "#{letter}#{number}".to_sym => nil }
+        { "#{letter}#{number}".to_sym => DEFAULT_VALUE }
       end
     end.flatten.reduce(:merge)
   end
--- lib/recommendation.rb
+++ lib/recommendation.rb
@@ -1,5 +1,5 @@
 module Recommendation
-  MIDDLE_LINES_CELLS = {
+  MIDDLE_LINE_NEIGHBOURS = {
     A2: [:A1, :A3],
     B1: [:A1, :C1],
     B3: [:A3, :C3],
@@ -45,6 +45,22 @@ module Recommendation
   end
 
   private
+  
+  def available_moves
+    grid.available_cells_coordinates
+  end
+
+  def middle_coordinate
+    grid.middle_coordinate
+  end
+
+  def middle_line_coordinates
+    grid.middle_line_coordinates
+  end
+
+  def corner_coordinates
+    grid.corner_coordinates
+  end
 
   def nobody_moved_yet?
     grid.coordinates.all? { |coord| !grid.matrix[coord].has_content? }
@@ -55,29 +71,29 @@ module Recommendation
   end
 
   def any_middle_line_cell_free?
-    grid.middle_line_coordinates.any? { |coord| !grid.matrix[coord].has_content? }
+    middle_line_coordinates.any? { |coord| !grid.matrix[coord].has_content? }
   end
 
   def middle_is_mine?
-    grid.get_content(grid.middle_coordinate).content == marker
+    grid.get_content(middle_coordinate).content == marker
   end
 
   def recommend_a_corner
-    coord = grid.corner_coordinates.sample
-    until grid.available_cells_coordinates.include?(coord) do
-      coord = grid.corner_coordinates.sample
+    coord = corner_coordinates.sample
+    until available_moves.include?(coord) do
+      coord = corner_coordinates.sample
     end
     coord
   end
 
   def recommend_middle
-    grid.middle_coordinate
+    middle_coordinate
   end
 
   def recommend_the_middle_of_a_line
-    coord = grid.middle_line_coordinates.sample
-    until grid.available_cells_coordinates.include?(coord) do
-      coord = grid.middle_line_coordinates.sample
+    coord = middle_line_coordinates.sample
+    until available_moves.include?(coord) do
+      coord = middle_line_coordinates.sample
     end
     coord
   end
@@ -87,18 +103,18 @@ module Recommendation
   end
 
   def recommend_an_empty_cell
-    grid.available_cells_coordinates.sample
+    available_moves.sample
   end
 
   def recommend_corner_in_proximity_of(coord)
-    MIDDLE_LINES_CELLS[coord].sample
+    MIDDLE_LINE_NEIGHBOURS[coord].sample
   end
 
   def find_which_middle_line
-    grid.middle_line_coordinates.select { |coord| grid.get_content(coord).has_content? }.first
+    middle_line_coordinates.select { |coord| grid.get_content(coord).has_content? }.first
   end
 
   def enemy_took_middle_line?
-    grid.middle_line_coordinates.any? { |coord| grid.get_content(coord).has_content? }
+    middle_line_coordinates.any? { |coord| grid.get_content(coord).has_content? }
   end
 end
